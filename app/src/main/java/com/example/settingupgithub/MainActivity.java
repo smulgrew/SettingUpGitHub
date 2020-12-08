9840e38450da88aa3e2c265d378bf9d87021c96d
Charlie <34101375+smulgrew@users.noreply.github.com>
2020-12-08 16:39:34 +0000

    2nd Commit

--- app/src/main/java/com/example/settingupgithub/MainActivity.java
+++ app/src/main/java/com/example/settingupgithub/MainActivity.java
@@ -10,5 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //kjshfkshfksdhfsahflksjhfahldfahjsd
+
+        System.out.println("Update Existing GitHub file.");
     }
 }
